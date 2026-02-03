d4e6b7abe14b57c4178d862662bf6c56bd760a68
Тимур Клыга <a37529@MacBookPro.bbrouter>
2026-02-03 13:46:23 +0300

    Add base styles for HTML elements

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,23 +1,25 @@
+import { Link } from "react-router-dom";
+
 function Header({ logoSrc, logoAlt }) {
   return (
     <header className="site-header">
-  <div className="site-header__inner">
-    <a className="site-header__brand" href="/">
-      <img className="site-header__logo" src={logoSrc} alt={logoAlt} />
-    </a>
+      <div className="site-header__inner">
+        <Link className="site-header__brand" to="/">
+          <img className="site-header__logo" src={logoSrc} alt={logoAlt} />
+        </Link>
 
-    <nav className="site-header__nav" aria-label="Primary Navigation">
-      <ul className="site-header__menu">
-        <li><a href="/">Home</a></li>
-        <li><a href="/about">About</a></li>
-        <li><a href="/menu">Menu</a></li>
-        <li><a href="/reservations">Reservations</a></li>
-        <li><a href="/order_online">Order Online</a></li>
-        <li><a href="/login">Login</a></li>
-      </ul>
-    </nav>
-  </div>
-</header>
+        <nav className="site-header__nav" aria-label="Primary Navigation">
+          <ul className="site-header__menu">
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/about">About</Link></li>
+            <li><Link to="/menu">Menu</Link></li>
+            <li><Link to="/reservations">Reservations</Link></li>
+            <li><Link to="/order_online">Order Online</Link></li>
+            <li><Link to="/login">Login</Link></li>
+          </ul>
+        </nav>
+      </div>
+    </header>
   );
 }
 
